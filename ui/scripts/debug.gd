4290706a6fba14bd410a2b50f1d17c7d35376c41
Foaming-Hippo <lincoln.frost1@gmail.com>
2025-09-10 13:42:48 -0600

    added debug

---
+++ ui/scripts/debug.gd
@@ -0,0 +1,30 @@
+extends PanelContainer
+
+@onready var property_container = %VBoxContainer
+
+var property
+var fps : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	visible = false
+	
+	add_debug_property("FPS",fps)
+	
+func _process(delta):
+	
+	fps = "%.2f" % (1.0/delta)
+	
+	property.text = property.name + ": " + fps
+	
+func _input(event):
+	#open debug
+	if event.is_action_pressed("debug"):
+		visible = !visible
+
+func add_debug_property(title: String,value):
+	property = Label.new()
+	property_container.add_child(property)
+	property.name = title
+	property.text = property.name + value
